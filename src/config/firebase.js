77943d5860f4227b537e564b24da469eba7ae02f
gituser708 <donmania999@gmail.com>
2025-09-07 20:00:11 +0530

    firebase API Key fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZfpNRfSxj9ea-6U6oe_nosmxzvOfoqaM",
+  apiKey: "AIzaSyAom6uizpTGh6imYru-ur0alZ8tJS_VbLI",
   authDomain: "gemai-project-83ebe8.firebaseapp.com",
   projectId: "gemai-project-83ebe8",
   storageBucket: "gemai-project-83ebe8.appspot.com",
